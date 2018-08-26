1feb2307526395e6ff67b39c23cb353747fe04df
asunwei <993525033@qq.com>
2018-08-26 11:01:50 +0800

    333

--- javabasic/src/test/java/com/learn/demo/JavaDemo.java
+++ javabasic/src/test/java/com/learn/demo/JavaDemo.java
@@ -1,15 +1,26 @@
 package com.learn.demo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JavaDemo {
     public static void main(String[] args) {
         System.out.println("===============================");
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "one");
+        map.put(2, "one");
+        map.put(3, "one");
+        System.out.println(map);
+
         List<String> list = new ArrayList<String>();
         list.add("one");
         list.add("one");
         list.add("one");
+        list.add("two");
+        list.add("two");
+        list.add("two");
         System.out.println(list);
     }
 }
