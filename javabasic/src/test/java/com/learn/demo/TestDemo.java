8506dd2917a2a3e1c59c847bcd286963ed3b5bc8
asunwei <993525033@qq.com>
2018-12-30 15:05:54 +0800

    no message

--- javabasic/src/test/java/com/learn/demo/TestDemo.java
+++ javabasic/src/test/java/com/learn/demo/TestDemo.java
@@ -17,7 +17,7 @@ public class TestDemo {
 
         System.out.println("hello java");
         System.out.println("TestDemo");
-        System.out.println("this is test demo2");
+        System.out.println("this is test demo3");
     }
 
 }
