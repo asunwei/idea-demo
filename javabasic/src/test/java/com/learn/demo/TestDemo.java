10fe896c2366e8032c5af2233f7018cc2917245b
asunwei <993525033@qq.com>
2018-12-30 15:03:02 +0800

    add

--- javabasic/src/test/java/com/learn/demo/TestDemo.java
+++ javabasic/src/test/java/com/learn/demo/TestDemo.java
@@ -18,6 +18,7 @@ public class TestDemo {
         System.out.println("hello java");
         System.out.println("TestDemo");
         System.out.println("this is test demo");
+        System.out.println("this is test demo2");
     }
 
 }
